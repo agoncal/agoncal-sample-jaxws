4d49b6dc9006d13c52f961bc2b7f48de458a1e94
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-06-19 18:41:07 +0200

    First commit

---
+++ 01-EndPoints/src/main/java/org/agoncal/sample/jaxws/endpoint/HelloEJBEndpoint.java
@@ -0,0 +1,23 @@
+package org.agoncal.sample.jaxws.endpoint;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@WebService
+@Stateless
+public class HelloEJBEndpoint {
+
+
+    // ======================================
+    // =          Business methods          =
+    // ======================================
+
+    public String saySomething(String something) {
+        return "The HelloEJBEndpoint is saying : " + something;
+    }
+}
---
+++ 01-EndPoints/src/main/java/org/agoncal/sample/jaxws/endpoint/HelloServletEndpoint.java
@@ -0,0 +1,21 @@
+package org.agoncal.sample.jaxws.endpoint;
+
+import javax.jws.WebService;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@WebService
+public class HelloServletEndpoint {
+
+
+    // ======================================
+    // =          Business methods          =
+    // ======================================
+
+    public String saySomething(String something) {
+        return "The HelloServletEndpoint is saying : " + something;
+    }
+}
